7fa61d454c32bfbc62046f6c8881a950ebf43689
pradeepngpd <pradeep.ngpd@gmail.com>
2026-08-10 21:27:02 +0530

    Separated html files and saved separately

--- PracticeJS.js
+++
@@ -1,6 +0,0 @@
-let clickBtn = document.getElementById("ClickBtn");
-let displayMessage = document.getElementById("displayMessage");
-clickBtn.addEventListener("click", function() {
-    displayMessage.textContent = "You clicked the button! Code wrote without seeing the reference file.";
-    displayMessage.style.color = "blue";
-});
